fb84ae8768202aaef7b1bc501ea72c73cb7ea43b
Lingsat <lingsat@bigmir.net>
2020-02-15 09:06:18 +0200

    First commit

---
+++ lesson_1/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+
+/* let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);*/
+
+let arr = ['plum.png','orange.jpg','apple.bmp']
+
+//console.log(arr[2]);
+
+// alert("Hello World!");
+
+/* let answer = +prompt("Есть ли Вам 18 лет?", "Да");
+console.log(typeof(answer));
+
+console.log("arr" + " - object");
+console.log(4 + " - object"); */
+
+/* let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2); */
+
+let isCheked = true,
+    isCloze = true;
+
+console.log(isCheked && isCloze);
+
