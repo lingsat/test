4149dae902ccdf537bb4a1547abd597f642dab58
Lingsat <lingsat@bigmir.net>
2020-02-20 16:59:43 +0200

    Массивы2

--- lesson_2/script.js
+++ lesson_2/script.js
@@ -1,19 +1,61 @@
-let num = 50;
+/* let options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
 
-/* while (num < 55) {
-    console.log(num);
-    num++;
+console.log(options.name);
+options.bool = false;
+options.colors = {
+    border: "black",
+    bg: "red"
+};
+
+delete options.bool;
+
+console.log(options);
+
+for (let key in options) {
+    console.log('Свойство ' + key + ' имеет значение ' + options[key]);
+};
+console.log(Object.keys(options).length);
+
+let arr = ["first", 2, 3, "four", 5];
+ */
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+
+arr.forEach(function (item, i, mass) {
+    console.log(i + ': ' + item + " (массив: " + mass + ")");
+});
+
+console.log(arr);
+
+let mass = [1, 3, 4, 6, 7];
+
+for (let key of mass) {
+    console.log(key);
 }; */
 
-/* do {
-    console.log(num);
-    num++;
-}
-while (num < 55); */
+/* let ans = prompt("", ""),
+    arr = [];
 
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        continue;
-    };
-    console.log(i);
-};
+arr = ans.split(',');
+
+console.log(arr);  */
+
+/* let arr = ['aawe', 'zzz', 'pp', 'rqa'],
+    i = arr.join(', ');
+
+console.log(i[3]);
+ */
+let arr = [1, 15, 4],
+    i = arr.sort(compareNum);
+
+function compareNum(a,b) {
+    return a-b;
+}
+
+console.log(arr);
